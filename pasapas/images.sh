6344ab7be03b542529cceca8e44744a03ee6700c
François Di Paola <francois.dipaola@cea.fr>
2024-09-17 16:50:35 +0200

    Fin de l'exercice 1 : poutre avec force suiveuse
    - Slides
    - Animations des déformées de la poutre
    - Ajout d'un répertoire images/exo/
    - Petites modif du script images.sh

--- pasapas/images.sh
+++ pasapas/images.sh
@@ -22,16 +22,17 @@ cd images/
 echo "[2/5] Traitement des images .ps (retrait des légendes, titres ...)"
 for i in *.ps
 do
-# pour les fichiers ci-dessous : retrait du titre et legende, déplacement isovaleurs, changement taille fonte et epaisseur trait
-  if [ "${i}" = "exemple_0.ps" ]
+# pour les fichiers ci-dessous : retrait titre + legende, déplacement isovaleurs, changement taille fonte + epaisseur trait
+  if [ "${i}" = "exemple_0.ps" ] ||
+     [[ "${i}" == exo_1_*.ps ]]
   then
     cast-post.sh -o -t -c 2 -s 15 -l 10 ${i}
-# pour les fichiers ci-dessous : retrait du titre, déplacement isovaleurs, changement taille fonte et epaisseur trait
+# pour les fichiers ci-dessous : retrait titre, déplacement isovaleurs, changement taille fonte + epaisseur trait
   elif [ "${i}" = "exemple_1.ps" ] ||
        [ "${i}" = "exemple_2.ps" ]
   then
     cast-post.sh -o -t -c 1 -s 15 -l 10 ${i}
-# et pour tous les autres : retrait du titre, déplacement isovaleurs, changement taille fonte
+# et pour tous les autres : retrait titre, déplacement isovaleurs, changement taille fonte
   else
     cast-post.sh -o -t -c 1 -s 15 ${i}
   fi
@@ -67,5 +68,7 @@ done
 
 
 # Ménage des fichiers
+mv -f exemple*.pdf exemple/.
+mv -f exo*.pdf exo/.
 rm *.ps *.eps
 cd ..
